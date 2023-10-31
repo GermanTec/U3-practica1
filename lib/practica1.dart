d6f371e1b0607762ef73b530a5804e5c478e38d0
GermanTec <georozcozu@ittepic.edu.mx>
2023-10-31 11:23:38 -0700

    Primer commit

---
+++ lib/practica1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class P31 extends StatefulWidget {
+  const P31({super.key});
+
+  @override
+  State<P31> createState() => _P31State();
+}
+
+class _P31State extends State<P31> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
